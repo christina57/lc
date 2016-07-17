689db9dfb46fed4792f8b5df7a1a1d3b45be925e
zzren <christina198857@gmail.com>
2016-07-16 19:55:55 -0700

    wiggle sort

--- lc/src/lc/MinStack.java
+++ lc/src/lc/MinStack.java
@@ -3,39 +3,39 @@ package lc;
 import java.util.LinkedList;
 
 class MinStack {
-	
-	private LinkedList<Integer> stack = new LinkedList<Integer>();
-	private LinkedList<Integer> minstack = new LinkedList<Integer>();
-	
-    public void push(int x) {
-    	stack.add(x);
-    	if(minstack.isEmpty() || x <= minstack.getLast()){
-    		minstack.add(x);
-    	}
-    }
 
-    public void pop() {
-    	if(stack.isEmpty()){
-    		return;
-    	}
-        int num = stack.removeLast();
-        if(minstack.getLast() == num){
-        	minstack.removeLast();
-        }
-    }
+	private Stack<Integer> s;
+	private int min;
 
-    public int top() {
-    	if(stack.isEmpty()){
-    		return Integer.MIN_VALUE;
-    	}
-        return stack.getLast();
-    }
+	/** initialize your data structure here. */
+	public MinStack() {
+		s = new Stack<Integer>();
+		min = Integer.MAX_VALUE;
+	}
 
-    public int getMin() {
-    	if(minstack.isEmpty()){
-    		return Integer.MIN_VALUE;
-    	}
-        return minstack.getLast();
-    }
+	public void push(int x) {
+		if(x <= min){
+			s.push(min);
+			min = x;
+		}
+		s.push(x);
+	}
+
+	public void pop() {
+		if(s.peek() == min){
+			s.pop();
+			min = s.pop();
+		} else {
+			s.pop();
+		}
+	}
+
+	public int top() {
+		return (int)s.peek();
+	}
+
+	public int getMin() {
+		return min;
+	}
 }
 
